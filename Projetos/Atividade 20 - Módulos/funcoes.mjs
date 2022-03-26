36b2e30a75c9d13f1a44aede23570dc8a7899eca
rudaaranha <rudaaranha@gmail.com>
2022-03-26 00:30:12 -0300

    Adicionando o exercício sobre módulos em JS

---
+++ Projetos/Atividade 20 - Módulos/funcoes.mjs
@@ -0,0 +1,17 @@
+function MostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`
+}
+
+function MostraCidade(nome, cidade) {
+    return `A cidade de ${nome} é ${cidade}`
+}
+
+function MostraHobby(nome, hobby) {
+    return `O hobby de ${nome} é ${hobby}`
+}
+
+export {
+    MostraIdade,
+    MostraCidade,
+    MostraHobby
+}
---
+++ Projetos/Atividade 20 - Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { MostraIdade, MostraCidade, MostraHobby } from "./funcoes.mjs";
+
+console.log(MostraIdade('Rudá', 32));
+console.log(MostraCidade('Rudá', 'Campina Grande'));
+console.log(MostraHobby('Rudá', 'jogar'));
